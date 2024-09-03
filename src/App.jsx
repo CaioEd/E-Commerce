7466fed4b4f252ac3bf4982415c0869dbbf8ef30
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2024-09-03 19:49:40 -0300

    Atualizações projeto

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
+// Pages
 import Home from './pages/Home'
+import ProductPage from './Pages/ProductPage'
 
 function App() {
   
@@ -11,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/product' element={<ProductPage />} />
         </Routes>
       </BrowserRouter>
     </>
